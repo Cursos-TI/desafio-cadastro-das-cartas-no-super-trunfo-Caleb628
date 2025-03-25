cb218f4a515c44038ce432d22cfff761e3b74d5e
Caleb <139576267+Caleb628@users.noreply.github.com>
2025-03-24 21:22:06 -0300

    Update CartasSuperTrunfo.c
    first changes to level one

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,43 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    
+    char nome_da_cidade1[100], estado1[1],  codigo1[10];
+    char  nome_da_cidade2[100], estado2[1], codigo2[10];
+    int populacao1, p_turisticos1;
+    int populacao2, p_turisticos2;
+    float area1, pib1;
+    float area2, pib2;
+    
+    
+
+    printf("Estado, a variavel deve ser uma letra maiuscula: ");
+    scanf("%s", &estado);
+    printf("Código da Carta, a letra do estado seguida de um número de 01 a 04: ");
+    scanf("%s", &codigo);
+    printf("nome da cidade: ");
+    scanf("%s", &nome_da_cidade);
+    printf("População, o número de habitantes da cidade: ");
+    scanf("%d", &populacao);
+    printf("A área da cidade em quilômetros quadrados: ");
+    scanf("%f", &area);
+    printf("O Produto Interno Bruto da cidade: ");
+    scanf("%f", &pib);
+
+    printf("card1");
+
+     printf("Estado, a variavel deve ser uma letra maiuscula: ");
+    scanf("%s", &estado1);
+    printf("Código da Carta, a letra do estado seguida de um número de 01 a 04: ");
+    scanf("%s", &codigo);
+    printf("nome da cidade: ");
+    scanf("%s", &nome_da_cidade);
+    printf("População, o número de habitantes da cidade: ");
+    scanf("%d", &populacao);
+    printf("A área da cidade em quilômetros quadrados: ");
+    scanf("%f", &area);
+    printf("O Produto Interno Bruto da cidade: ");
+    scanf("%f", &pib);
+
     return 0;
 }
